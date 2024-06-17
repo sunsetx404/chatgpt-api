ebde8c3650d4876cf1b70c70d2724fc08350c366
sunsetx404 <125448451+sunsetx404@users.noreply.github.com>
2024-06-17 23:52:49 +0800

    Update nubit.sh

--- nubit.sh
+++ nubit.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+# 更新包列表
 sudo apt-get update
-sudo apt-get install screen
-screen -S nubit
-curl -sL1 https://nubit.sh | bash
+
+# 安装screen
+sudo apt-get install -y screen
+
+# 创建一个名为nubit的screen会话并在其中执行curl命令
+screen -S nubit -dm bash -c 'curl -sL https://nubit.sh | bash'
