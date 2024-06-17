ca94a33b166ae8a95150eae1e0074f9b1cb67780
sunsetx404 <125448451+sunsetx404@users.noreply.github.com>
2024-06-17 23:53:31 +0800

    Update nubit.sh

--- nubit.sh
+++ nubit.sh
@@ -8,3 +8,6 @@ sudo apt-get install -y screen
 
 # 创建一个名为nubit的screen会话并在其中执行curl命令
 screen -S nubit -dm bash -c 'curl -sL https://nubit.sh | bash'
+
+# 输出提示信息
+echo "Nubit正在screen会话中运行。使用'screen -r nubit'命令查看会话。"
